14340daa724b5ce2c925ceb359e5ca3d49deb6cd
leochocolat <l.mouraire@outlook.fr>
2021-03-01 15:15:17 +0100

    :art: Fix resource loader

--- utils/ResourceLoader.js
+++ utils/ResourceLoader.js
@@ -4,16 +4,13 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 // Utils
-import EventDispatcher from '@/utils/EventDispatcher';
-import Stopwatch from '@/utils/Stopwatch';
+import EventDispatcher from './EventDispatcher';
+import Stopwatch from './Stopwatch';
 import resources from '@/resources';
 
 // States
 const STATE_LOADING = 'loading';
 const STATE_LOADED = 'loaded';
-
-// Cache
-const cache = [];
 
 export default class ResourceLoader extends EventDispatcher {
     constructor(resources, basePath) {
@@ -28,14 +25,21 @@ export default class ResourceLoader extends EventDispatcher {
     /**
      * Static
      */
+    static cache = [];
+
     static get(name) {
         const resource = this._getResourceByName(name);
         return resource.data;
     }
 
+    static setResource(resource) {
+        ResourceLoader.cache = resource;
+        this._allAssetsLoaded = true;
+    }
+
     static _getResourceByName(name) {
-        for (let i = 0, len = cache.length; i < len; i++) {
-            if (cache[i].name === name) return cache[i];
+        for (let i = 0, len = ResourceLoader.cache.length; i < len; i++) {
+            if (ResourceLoader.cache[i].name === name) return ResourceLoader.cache[i];
         }
         return undefined;
     }
@@ -43,46 +47,31 @@ export default class ResourceLoader extends EventDispatcher {
     /**
      * Private
      */
-    _deepClone(array) {
-        return JSON.parse(JSON.stringify(array));
-    }
-
     _loadResources() {
+        const promises = [];
+
         for (let i = 0, len = this._resources.length; i < len; i++) {
-            this._loadResource(this._resources[i]);
+            promises.push(this._loadResource(this._resources[i]));
         }
+
+        return Promise.all(promises).then((responses) => {
+            ResourceLoader.cache = responses;
+            this.dispatchEvent('complete', responses);
+        });
     }
 
     _loadResource(resource) {
         switch (resource.type) {
-            case 'image': {
-                this._loadImage(resource);
-                break;
-            }
-            case 'font': {
-                this._loadFont(resource);
-                break;
-            }
+            case 'image':
+                return this._loadImage(resource);
+            case 'font':
+                return this._loadFont(resource);
             case 'gltf':
-            case 'glb': {
-                this._loadGltf(resource);
-                break;
-            }
-            case 'draco': {
-                this._loadDraco(resource);
-                break;
-            }
+            case 'glb':
+                return this._loadGltf(resource);
+            case 'draco':
+                return this._loadDraco(resource);
         }
-    }
-
-    _checkResourcesStatus() {
-        for (let i = 0, len = this._resources.length; i < len; i++) {
-            if (this._resources[i].state === STATE_LOADING) {
-                return;
-            }
-        }
-        this._allAssetsLoaded = true;
-        this.dispatchEvent('complete');
     }
 
     /**
@@ -90,27 +79,48 @@ export default class ResourceLoader extends EventDispatcher {
      */
     _loadImage(resource) {
         resource.state = STATE_LOADING;
+        const stopWatch = new Stopwatch();
+        stopWatch.start();
+
         const image = new Image();
         image.crossOrigin = '';
-        image.onload = () => {
-            resource.state = STATE_LOADED;
-            cache.push(resource);
-            this._checkResourcesStatus();
-        };
+
+        const promise = new Promise((resolve) => {
+            image.onload = () => {
+                resource.state = STATE_LOADED;
+                stopWatch.stop();
+                resource.loadingDuration = `${stopWatch.duration}ms`;
+
+                resolve(resource);
+            };
+        });
+
         image.src = resource.path ? this._basePath + resource.path : resource.absolutePath;
         resource.data = image;
+
+        return promise;
     }
 
     _loadFont(resource) {
         resource.state = STATE_LOADING;
+        const stopWatch = new Stopwatch();
+        stopWatch.start();
+
         const observer = new FontFaceObserver(resource.name, {
             weight: resource.weight,
         });
-        observer.load().then(() => {
-            resource.state = STATE_LOADED;
-            cache.push(resource);
-            this._checkResourcesStatus();
+
+        const promise = new Promise((resolve) => {
+            observer.load().then(() => {
+                resource.state = STATE_LOADED;
+                stopWatch.stop();
+                resource.loadingDuration = `${stopWatch.duration}ms`;
+
+                resolve(resource);
+            });
         });
+
+        return promise;
     }
 
     _loadGltf(resource) {
@@ -120,7 +130,6 @@ export default class ResourceLoader extends EventDispatcher {
 
         const dracoLoader = new DRACOLoader();
         dracoLoader.setDecoderPath(this._basePath + '/libs/draco/');
-        dracoLoader.setDecoderConfig({ type: 'js' });
 
         const loader = new GLTFLoader();
         loader.setDRACOLoader(dracoLoader);
@@ -146,7 +155,6 @@ export default class ResourceLoader extends EventDispatcher {
 
         const dracoLoader = new DRACOLoader();
         dracoLoader.setDecoderPath(this._basePath + '/libs/draco/');
-        dracoLoader.setDecoderConfig({ type: 'js' });
 
         const loader = new GLTFLoader();
         loader.setDRACOLoader(dracoLoader);
@@ -163,5 +171,12 @@ export default class ResourceLoader extends EventDispatcher {
         });
 
         return promise;
+    }
+
+    /**
+     * Utils
+     */
+    _deepClone(array) {
+        return JSON.parse(JSON.stringify(array));
     }
 }
