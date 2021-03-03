d4e69046850f695ee3c7d47b47b045129555abff
leochocolat <l.mouraire@outlook.fr>
2021-03-03 16:30:01 +0100

    :sparkles: WebGL > Create Debug Camera

---
+++ webgl/objects/Cameras.js
@@ -0,0 +1,98 @@
+// Vendor
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+class Cameras {
+    constructor(options) {
+        this._renderer = options.renderer;
+        this._isActive = options.isActive;
+        this._width = options.width;
+        this._height = options.height;
+        this._debugger = options.debugFolder;
+
+        this._mainCamera = this._createMainCamera();
+        this._debugCamera = this._createDebugCamera();
+        this._isDebug = !!this._debugger;
+        this._active = this._isDebug ? this._debugCamera : this._mainCamera;
+        this._orbitControls = this._createOrbitControls();
+
+        this._debugFolder = this._createDebugFolder();
+    }
+
+    /**
+     * Public
+     */
+    get active() {
+        return this._active;
+    }
+
+    get main() {
+        return this._mainCamera;
+    }
+
+    get debug() {
+        return this._debugCamera;
+    }
+
+    setActive() {
+        this._isActive = true;
+        this._setActiveCamera();
+    }
+
+    setInactive() {
+        this._isActive = false;
+        this._setActiveCamera();
+    }
+
+    resize(width, height) {
+        this._width = width;
+        this._height = height;
+
+        this._mainCamera.aspect = this._width / this._height;
+        this._mainCamera.updateProjectionMatrix();
+
+        this._debugCamera.aspect = this._width / this._height;
+        this._debugCamera.updateProjectionMatrix();
+    }
+
+    /**
+     * Private
+     */
+    _createMainCamera() {
+        const camera = new THREE.PerspectiveCamera(75, this._width / this._height, 0.1, 1000);
+        camera.position.z = 2;
+
+        return camera;
+    }
+
+    _createDebugCamera() {
+        const camera = new THREE.PerspectiveCamera(75, this._width / this._height, 0.1, 1000);
+        camera.position.z = 2;
+
+        return camera;
+    }
+
+    _createOrbitControls() {
+        const orbitControls = new OrbitControls(this._debugCamera, this._renderer.domElement);
+        orbitControls.enabled = this._isDebug && this._isActive;
+        return orbitControls;
+    }
+
+    _setActiveCamera() {
+        this._active = this._isDebug ? this._debugCamera : this._mainCamera;
+        this._orbitControls.enabled = this._isDebug && this._isActive;
+    }
+
+    _createDebugFolder() {
+        if (!this._debugger) return;
+
+        const debugFolder = this._debugger.addFolder({ title: 'Cameras', expanded: false });
+        debugFolder.addInput(this, '_isDebug', { label: 'isDebug' }).on('change', () => {
+            this._setActiveCamera();
+        });
+
+        return debugFolder;
+    }
+}
+
+export default Cameras;
--- webgl/scenes/RenderTargetScene.js
+++ webgl/scenes/RenderTargetScene.js
@@ -1,11 +1,12 @@
 // Vendor
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Data
 import data from '../data';
-import ResourceLoader from '@/utils/ResourceLoader';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// Object
+import Cameras from '../objects/Cameras';
 
 class RenderTargetScene extends THREE.Scene {
     constructor(options) {
@@ -13,28 +14,25 @@ class RenderTargetScene extends THREE.Scene {
 
         this._name = options.name;
         this._id = options.id;
+        this._renderer = options.renderer;
         this._debugger = options.debugger;
         this._width = options.width;
         this._height = options.height;
         this._isActive = options.isActive;
-        this._renderer = options.renderer;
         this.background = new THREE.Color(data.colors[this._id]);
 
         this._renderTarget = this._createRenderTarget();
-        this._camera = this._createCamera();
         this._debugCube = this._createDebugCube();
-
-        this._orbitControls = this._createOrbitControls();
         this._ambientLight = this._createAmbientLight();
-
         this._debugFolder = this._createDebugFolder();
+        this._cameras = this._createCameras();
     }
 
     /**
      * Public
      */
     get camera() {
-        return this._camera;
+        return this._cameras.active;
     }
 
     get renderTarget() {
@@ -51,12 +49,12 @@ class RenderTargetScene extends THREE.Scene {
 
     setActive() {
         this._isActive = true;
-        this._orbitControls.enabled = true;
+        this._cameras.setActive();
     }
 
     setInactive() {
         this._isActive = false;
-        this._orbitControls.enabled = false;
+        this._cameras.setInactive();
     }
 
     update(time, delta) {
@@ -71,9 +69,7 @@ class RenderTargetScene extends THREE.Scene {
         this._height = height;
 
         this._renderTarget.setSize(this._width, this._height);
-
-        this._camera.aspect = this._width / this._height;
-        this._camera.updateProjectionMatrix();
+        this._cameras.resize(width, height);
     }
 
     /**
@@ -85,17 +81,17 @@ class RenderTargetScene extends THREE.Scene {
         return renderTarget;
     }
 
-    _createCamera() {
-        const camera = new THREE.PerspectiveCamera(75, this._width / this._height, 0.1, 1000);
-        camera.position.z = 2;
+    _createCameras() {
+        const cameras = new Cameras({
+            renderer: this._renderer,
+            isActive: this._isActive,
+            debugger: this._debugger,
+            debugFolder: this._debugFolder,
+            width: this._width,
+            height: this._height,
+        });
 
-        return camera;
-    }
-
-    _createOrbitControls() {
-        const orbitControls = new OrbitControls(this._camera, this._renderer.domElement);
-        orbitControls.enabled = false;
-        return orbitControls;
+        return cameras;
     }
 
     _createDebugCube() {
